3eb35a010810e40595d60b7778b97834eb8e7655
Mayisha Muntaha Alam <mayisha.alam@northsouth.edu>
2021-12-19 23:33:01 +0600

    Initial commit

---
+++ search.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search Box</title>
+	<link rel="stylesheet" type="text/css" href="search.css">
+</head>
+<body>
+	<div class ="search"> 
+		<input type="text" name="search" placeholder="Search here">
+	</div>
+</body>
+</html>
